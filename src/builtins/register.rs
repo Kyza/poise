336e5dfd12f9c73cff12fc7c19029eca9a142f78
jamesbt365 <jamesbt365@gmail.com>
2025-04-18 20:41:41 +0100

    builtins

--- src/builtins/register.rs
+++ src/builtins/register.rs
@@ -179,7 +179,14 @@ pub async fn register_application_commands_buttons<U: Send + Sync + 'static, E>(
             .emoji('🗑'),
     ];
 
+    #[cfg(feature = "unstable")]
+    let components = [serenity::CreateComponent::ActionRow(
+        serenity::CreateActionRow::buttons(&buttons),
+    )];
+
+    #[cfg(not(feature = "unstable"))]
     let components = [serenity::CreateActionRow::buttons(&buttons)];
+
     let builder = crate::CreateReply::default()
         .content("Choose what to do with the commands:")
         .components(&components);
